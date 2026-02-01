d53bb29988ae47aafa78bb2332226ba120c2656d
HeikesFootSlave <heikesfootslave@gmail.com>
2026-02-01 08:34:31 +0100

    CleanUp Code, Comments, locales

--- src/version.py
+++ src/version.py
@@ -4,5 +4,5 @@ Central version management for Steam Library Manager.
 
 __version__ = "1.0.0"
 __release_date__ = "2026-01-31"
-__author__ = "Switch-Bros"
+__author__ = "SwitchBros"
 __license__ = "MIT"
