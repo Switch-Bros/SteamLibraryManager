58103363db50c610c80223e919c346d63184fb0a
HeikesFootSlave <heikesfootslave@gmail.com>
2026-02-24 23:03:07 +0100

    Changed version for Release from 1.0.0 to 1.1.1
    Honor to SwitchBros first SwitchBros_BasisPaket which was number 111 ;)

--- src/version.py
+++ src/version.py
@@ -7,7 +7,7 @@ from __future__ import annotations
 __all__ = ["__app_name__", "__version__", "__release_date__", "__author__", "__license__"]
 
 __app_name__ = "Steam Library Manager"
-__version__ = "1.0.0"
+__version__ = "1.1.1"
 __release_date__ = "2026-01-31"
 __author__ = "SwitchBros"
 __license__ = "MIT"
